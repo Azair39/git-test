ed6e64d2bebad9ad6508859d9467849d8ede2d84
Tair Azatov <Tair Azatov>
2016-06-10 22:25:51 +0600

    updated Calculate.java

--- Calculate.java
+++ Calculate.java
@@ -1,7 +1,31 @@
 public class Calculate{
 	public static void main(String[] args)
 	{
+		int a = Integer.valueOf(args[0]);
+		int b = Integer.valueOf(args[2]);
+		String s = args[1];
+		float res = 0;
+		System.out.println(a + " " + s + " "+ b);
 		System.out.println("Calculating...");
+		switch(s)
+		{
+			case "+":
+				res = a + b;
+				break;
+			case "-":
+				res = a - b;
+				break;
+			case ".":
+				res = a * b;
+				break;
+			case "/": case ":":
+				res = a / b;
+				break;
+			default: 
+				res = 0;	
+				break;
+		}		
+		System.out.println("Result = " + res);
 		System.out.println("Done!");
 	}
 }
