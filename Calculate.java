eedb4444fc5f5383ad158a5fe631da4fc5896c20
Tair Azatov <Tair Azatov>
2016-06-11 19:35:39 +0600

    upd Calculate

--- Calculate.java
+++ Calculate.java
@@ -1,18 +1,57 @@
-public class Calculate{
-	public static void main(String[] args)
+	import java.lang.Math.*;
+	import java.io.BufferedReader;
+	import java.io.InputStreamReader;
+	
+public class Calculate{	
+	public static void main(String[] args) throws Exception
 	{
-		int a = Integer.valueOf(args[0]);
-		int b = Integer.valueOf(args[2]);
-		String s = args[1];
-		float answer = Calculation(Integer.valueOf(args[0]), args[1], Integer.valueOf(args[2]));
-		System.out.println("Result = " + answer);
-		System.out.println("Done!");
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		int a;
+		int b;
+		float answer = 0;
+		String s;
+		char[] chArray;
+		
+		
+		while (true)
+		{	
+			String[] array = {"", "", ""};
+			s = reader.readLine();
+			if (s.equals("exit"))
+			{	
+				System.out.println("...........................................");
+				System.out.println("Program finished");
+				break;
+			}
+			else
+			{
+				chArray = s.toCharArray();
+				int j = 0;
+				int i = 0;
+				while(i < s.length())
+				{
+					if (chArray[i] == ' ')
+					{
+						j+=1;
+						i+=1;
+						if (i == s.length())
+							break;
+					}
+					array[j] = array[j] + chArray[i];
+					i+=1;
+				}
+				if (j == 2)
+					answer = Calculation(Integer.parseInt(array[0]), array[1], Integer.parseInt(array[2]));
+				else
+					answer = Calculation(Math.round(answer), array[0], Integer.parseInt(array[1]));
+				System.out.println("Result = " + answer);	
+			}
+		}
 	}
 	
-	private static float Calculation(int a; String s; int b)
+	private static float Calculation(int a, String s, int b)
 	{
 		float res = 0;
-		System.out.println(a + " " + s + " "+ b);
 		System.out.println("Calculating...");
 		switch(s)
 		{
